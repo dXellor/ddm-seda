cecbf80f67faa6059cb9ee3a0e1bc1d5d880cd22
Nikola Simic <nikola7simic@gmail.com>
2025-03-16 21:30:50 +0100

    feat: add route guards (wip)

--- src/seda-ui/src/app/app.routes.ts
+++ src/seda-ui/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { SignUpInPageComponent } from './pages/sign-up-in-page/sign-up-in-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { authGuard } from './guards/auth.guard';
+import { anonGuard } from './guards/anon.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    pathMatch: 'full',
+    canActivate: [authGuard],
+  },
   {
     path: 'signupin',
     component: SignUpInPageComponent,
     pathMatch: 'full',
+    canActivate: [anonGuard],
   },
 ];
